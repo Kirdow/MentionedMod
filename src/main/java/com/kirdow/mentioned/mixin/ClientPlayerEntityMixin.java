88705bb264ed27d11b5f7a0f8e7cc33f30e87094
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-11-07 12:43:58 +0100

    update to 1.19.2

--- src/main/java/com/kirdow/mentioned/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/kirdow/mentioned/mixin/ClientPlayerEntityMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
 
-    @Inject(method = "sendChatMessagePacket(Lnet/minecraft/network/message/ChatMessageSigner;Ljava/lang/String;Lnet/minecraft/text/Text;)V", at = @At("HEAD"))
+    @Inject(method = "sendChatMessage(Ljava/lang/String;Lnet/minecraft/text/Text;)V", at = @At("HEAD"))
     private void chatMessagePacketHook(CallbackInfo ci) {
         Mentioned.skip(1000L);
     }
