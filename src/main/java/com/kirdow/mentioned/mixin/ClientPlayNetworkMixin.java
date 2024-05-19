ed7b15a86037eda721b5e8086ebe134e19c606e3
Kirdow <10155603+Kirdow@users.noreply.github.com>
2024-05-19 15:04:25 +0200

    Update 1.1 to 1.20.1 fabric

--- src/main/java/com/kirdow/mentioned/mixin/ClientPlayNetworkMixin.java
+++ src/main/java/com/kirdow/mentioned/mixin/ClientPlayNetworkMixin.java
@@ -1,16 +1,17 @@
 package com.kirdow.mentioned.mixin;
 
 import com.kirdow.mentioned.Mentioned;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerEntity.class)
-public class ClientPlayerEntityMixin {
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientPlayNetworkMixin {
 
-    @Inject(method = "sendChatMessage(Ljava/lang/String;Lnet/minecraft/text/Text;)V", at = @At("HEAD"))
+    @Inject(method = "sendChatMessage(Ljava/lang/String;)V", at = @At("HEAD"))
     private void chatMessagePacketHook(CallbackInfo ci) {
         Mentioned.skip(1000L);
     }
